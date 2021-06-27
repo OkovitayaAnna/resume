f1e60b74274f6bfe51a52dade7dd535f19661dd8
OkovitayaAnna <okovita.ann@gmail.com>
2021-06-27 15:29:27 +0200

    Fully made and improved buttons in skills

--- js/skills.js
+++ js/skills.js
@@ -1,13 +1,19 @@
-//delete
-var dlt = document.getElementById('dlt');
+//read
+function read(key) {
+    var value = JSON.parse(localStorage.getItem(key));
+    return value;
+}
 
+var arr = read('skills');
+if (arr) {
+    arr.forEach(element => {
+        newSkill = document.createElement('div');
+        newSkill.innerHTML = `<div id="${element[0]}"> <dt class="programming-language">${element[0]}</dt> <dd> <div class="skills-level">${element[1]}%</div> </dd> <div>`;
+        document.getElementById('dl').append(newSkill);
 
-dlt.addEventListener("click", function () {
-    var sd = document.getElementById('skill-d').value;
-
-    localStorage.removeItem(sd);
-    document.getElementById(sd).remove();
+        include("js/skills.colors.js");
 });
+}
 
 
 //create
@@ -16,13 +22,39 @@ var create = document.getElementById('crt');
 create.addEventListener("click", function () {
     var skill = document.getElementById('skill-c').value;
     var prct = document.getElementById('percentage').value;
+    var ob = read('skills');
+    if (ob) {var obj = ob;} else {var obj = [];};
+    var set = [skill, prct];
 
-    localStorage.setItem(skill, prct);
+    obj.push(set);
+    localStorage.setItem('skills', JSON.stringify(obj));
+
     newSkill = document.createElement('div');
     newSkill.innerHTML = `<div id="${skill}"> <dt class="programming-language">${skill}</dt> <dd> <div class="skills-level">${prct}%</div> </dd> <div>`;
     document.getElementById('dl').append(newSkill);
 
     include("js/skills.colors.js");
+});
+
+
+//delete
+var dlt = document.getElementById('dlt');
+
+
+dlt.addEventListener("click", function () {
+    var sd = document.getElementById('skill-d').value;
+    var remove = read('skills');
+    var i = 0;
+
+    remove.forEach(element => {
+        if (element[0] == sd) {remove.splice(i, 1)};
+        i++;
+
+        localStorage.setItem('skills', JSON.stringify(remove));
+    });
+
+    document.getElementById(sd).remove();
+    i = 0;
 });
 
 
@@ -32,8 +64,14 @@ var update = document.getElementById('updt');
 update.addEventListener("click", function () {
     var skill = document.getElementById('skill-u').value;
     var updt = document.getElementById('update').value;
+    var update = read('skills');
 
-    localStorage.setItem(skill, updt);
+    update.forEach(element => {
+        if (element[0] == skill) {element[1] = updt};
+
+        localStorage.setItem('skills', JSON.stringify(update));
+    });
+
     newSkill = document.createElement('div');
     newSkill.innerHTML = `<div  id="${skill}"> <dt class="programming-language">${skill}</dt> <dd> <div class="skills-level">${updt}%</div> </dd> </div>`;
     document.getElementById(skill).remove();
